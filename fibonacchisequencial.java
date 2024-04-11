2a043d5b0081613ab115b255ae9104f7832912e8
vrajmore <vrajvjm@gmail.com>
2024-04-11 11:58:10 +0530

    JAVA

--- fibonacchisequencial.java
+++ fibonacchisequencial.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class fibonacchisequencial {
     public static void main(String[] args) {
         int a = 0;
         int b = 1;
         int c = 1;
 
-        for (int i = 0; i < 11; i++) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter digit to print fibonacchi series");
+        int x = sc.nextInt();
+
+        for (int i = 0; i <= x; i++) {
             System.out.print(a+", ");
 
             a = b;
